68b52a129369a015b254fc3b15d2cc9d17c8e1b4
Maou <47064273+maou0@users.noreply.github.com>
2024-07-29 14:39:50 +0300

    Update set_domain.sh

--- set_domain.sh
+++ set_domain.sh
@@ -10,10 +10,11 @@ elif [[ -z "$token" ]]; then
     exit 1
 fi
 
+apt install openssl curl ca-certificates certbot -y
 hostnamectl set-hostname "$domain"
 curl "https://freemyip.com/update?token=${token}&domain=${domain}"
 (crontab -l; echo "30 2 * * 0 curl \"https://freemyip.com/update?token=${token}&domain=${domain}\"") | crontab -
 certbot certonly --standalone --agree-tos --register-unsafely-without-email -d "$domain"
-cp /etc/letsencrypt/live/${domain}/fullchain.pem cert/
-cp /etc/letsencrypt/live/${domain}/privkey.pem cert/
+cp /etc/letsencrypt/live/${domain}/fullchain.pem /root/xui/cert/
+cp /etc/letsencrypt/live/${domain}/privkey.pem /root/xui/cert/
 (crontab -l; echo "0 0 * * * [ \$((\$(date +\%s)/86400\%91)) -eq 0 ] && certbot renew && cp /etc/letsencrypt/live/${domain}/privkey.pem /root/xui/cert && cp /etc/letsencrypt/live/${domain}/fullchain.pem /root/xui/cert") | crontab -
